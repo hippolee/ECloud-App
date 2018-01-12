4f228645023cfec376579331502e4cbee5a20214
litfb <litfb@yonyou.com>
2018-01-12 16:50:45 +0800

    初始化工程

---
+++ ECloud/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ECloud
+//
+//  Created by litfb on 2018/1/12.
+//  Copyright © 2018年 Yonyou Network. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
